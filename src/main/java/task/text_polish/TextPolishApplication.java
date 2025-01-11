1602d2ea0c6a2a064b4a2a81fa379ff208c7ea2c
pejko <aleksandarpejkovic@hotmail.com>
2025-01-11 12:27:18 +0100

    first commit

---
+++ src/main/java/task/text_polish/TextPolishApplication.java
@@ -0,0 +1,13 @@
+package task.text_polish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextPolishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextPolishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/task/text_polish/TextPolishApplicationTests.java
@@ -0,0 +1,13 @@
+package task.text_polish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextPolishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
